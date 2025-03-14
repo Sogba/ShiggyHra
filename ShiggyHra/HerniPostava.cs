d4a21c61fe2c73c6482b7bb8162d9df231fdaf9b
KoblizekXD <janprokupek@centrum.cz>
2025-03-14 10:30:29 +0100

    Add base clsas

--- ShiggyHra/HerniPostava.cs
+++ ShiggyHra/HerniPostava.cs
@@ -8,5 +8,19 @@ namespace ShiggyHra
 {
     public class HerniPostava
     {
+        public string Jmeno { get; set; }
+        public int Level { get; set; } = 1;
+        public int PoziceX { get; set; } = 0;
+        public int PoziceY { get; set; } = 0;
+
+        public HerniPostava(string jmeno)
+        {
+            Jmeno = jmeno;
+        }
+
+        public override string ToString()
+        {
+            return $"Jmeno: {Jmeno}, Level: {Level}, Pozice: [{PoziceX}, {PoziceY}]";
+        }
     }
 }
