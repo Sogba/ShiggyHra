c27eef3406df80105077a0efea25be4eb2001aad
KoblizekXD <janprokupek@centrum.cz>
2025-03-14 10:54:41 +0100

    Hrac

--- ShiggyHra/Hrac.cs
+++ ShiggyHra/Hrac.cs
@@ -8,8 +8,36 @@ namespace ShiggyHra
 {
     public class Hrac : HerniPostava
     {
+        public string Specializace { get; set; }
+        private TypObliceje Oblicej { get; set; }
+        public TypVlasu Vlasy { get; set; }
+        public BarvaVlasu Barva { get; set; }
+        public int XP { get; set; } = 0;
+
+
         public Hrac(string jmeno) : base(jmeno)
         {
+        }
+
+        public enum TypObliceje
+        {
+            VelkyNos,
+            Usoplesk,
+            MakeUp
+        }
+
+        public enum TypVlasu
+        {
+            Drdol,
+            Culik,
+            Pleska
+        }
+
+        public enum BarvaVlasu
+        {
+            Kastanova,
+            Blond,
+            Cervena
         }
     }
 }
